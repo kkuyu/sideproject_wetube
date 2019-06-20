21b725b9770b9ecb249cf74acd90d025617f9d5f
kkuyu <devils67@naver.com>
2019-06-21 01:00:50 +0900

    Express Middlewares Setting

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
 const app = express();
 
 const PORT = 4000;
@@ -11,14 +16,13 @@ const handleHome = (req, res) => res.send("Hello form home")
 
 const handleProfile = (req, res) => res.send("Hello form profile")
 
-const betweenHome = (req, res, next) => {
-	console.log("Between");
-	next();
-}
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(helmet())
 
-app.use(betweenHome);
-
-app.get("/", betweenHome, handleHome);
+app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
 
