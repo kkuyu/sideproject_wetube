7e6a6ee6601420fce11f7c9f15313dda5c9ce7af
kkuyu <devils67@naver.com>
2019-06-29 03:17:36 +0900

    Log Out and Upload Controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,5 @@
 import { videos } from "../db";
+import routes from "../routes";
 
 export const home = (req, res) => res.render("home", { pageTitle: "Home", videos })
 export const search = (req, res) => {
@@ -7,7 +8,16 @@ export const search = (req, res) => {
 	} = req;
 	res.render("search", { pageTitle: "Search", searchingBy, videos });
 }
-export const upload = (req, res) => res.render("upload", { pageTitle: "Upload"});
+
+export const getUpload = (req, res) => res.render("upload", { pageTitle: "Upload"});
+export const postUpload = (req, res) => {
+	const {
+		body: { file, title, description }
+	} = req;
+	// TO DO: Upload And Save Video
+	res.redirect(routes.videoDetail(324393))
+}
+
 export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle: "Video Detail"});
 export const editVideo = (req, res) => res.render("editVideo", { pageTitle: "Edit Video"});
 export const deleteVideo = (req, res) => res.render("deleteVideo", { pageTitle: "Delete Video"});
